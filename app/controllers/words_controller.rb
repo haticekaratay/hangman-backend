d7e68d86e99dfd9f57c159127c75bcbeb1565cfe
Hatice <haticekaraty@gmail.com>
2021-08-10 23:31:34 -0400

    define show action to render json word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,15 @@
 class WordsController < ApplicationController
     def index
         words = Word.all
-        render json: words, only: [:name, :category]
+        render json: words, only: [:id, :name, :category]
+    end
+
+    def show
+        word = Word.find_by(id: params[:id])
+        if word
+            render json: word, except: [:created_at, :updated_at]
+        else
+            render json: {message: "word not found"}
+        end
     end
 end
