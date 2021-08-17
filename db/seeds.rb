1b6b7a68b19281bc1e4429d6bd824b7767636eef
Hatice <haticekaraty@gmail.com>
2021-08-16 22:57:36 -0400

    adds more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,29 @@
 
 Word.create([
     {name: "Forrest Gump",category: "movie"},
-    {name: "Back to the Future",category: "movie"},
     {name: "The Shawshank Redemption",category: "movie"},
     {name: "The Godfather",category: "movie"},
     {name: "The Matrix",category: "movie"},
     {name: "Gladiator",category: "movie"},
-    {name: "Jurassic Park ",category: "movie"},
+    {name: "Jurassic Park",category: "movie"},
     {name: "The Dark Knight",category: "movie"},
     {name: "Interstellar",category: "movie"},
     {name: "Inception",category: "movie"},
     {name: "Star Wars",category: "movie"},
     {name: "Saving Private Ryan",category: "movie"},
     {name: "Life Is Beautiful",category: "movie"},
-    {name: "The Silence of the Lambs",category: "movie"},
     {name: "Joker",category: "movie"},
     {name: "Shutter Island",category: "movie"},
-    {name: "Toy Story",category: "movie"} ])
+    {name: "Toy Story",category: "movie"},
+    {name: "Marie Curie", category: "scientists/mathematician"},
+    {name: "Isaac Newton", category: "scientists/mathematician"},
+    {name: "Albert Einstein", category: "scientists/mathematician"},
+    {name: "Neil deGrasse Tyson", category:"scientists/mathematician"},
+    {name: "Stephen Hawking", category:"scientists/mathematician"},
+    {name: "Nikola Tesla", category:"scientists/mathematician"}
+])
 
+Player.create(name: "Haley")
+Player.create(name: "Jack")
+Game.create(player_id: 1, score: 330)
+Game.create(player_id: 2, score: 280)
