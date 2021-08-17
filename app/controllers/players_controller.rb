c71951d6126ba823d811bf3197c8ea949c4d7ae5
Hatice <haticekaraty@gmail.com>
2021-08-16 22:58:41 -0400

    add create index and show actions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,22 @@
 class PlayersController < ApplicationController
+    def index
+        players = Player.all
+        games = Game.all 
+        render json: players.to_json(:include => {
+            :games => {:only => [:score]},
+          }, :only => [:id,:name])
+    end
+
+    def create
+        Player.create(name: params[:name])
+    end
+
+    def show
+        player = Player.find_by(id: params[:id])
+        if player
+           render json: player, only: [:id, :name]
+        else
+            render json: {message: "Player not found"}
+        end
+    end
 end
