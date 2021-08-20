8566249a48f585800747252355c10e3007a3e98b
Hatice <haticekaraty@gmail.com>
2021-08-19 21:29:05 -0400

    render all games  including associated player data

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+
+    def index
+        games = Game.all
+        render json: games.to_json(:include => {
+                :player => {:only => [:name,:id]},
+              }, :only => :score)
+    end
+
+  
+   
 end
