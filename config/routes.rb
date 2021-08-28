cc2eca41403ce37c643edfdbb96506b91efc9f81
Hatice <haticekaraty@gmail.com>
2021-08-27 23:37:10 -0400

    remove routes that are not used

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :words
-  resources :games
-  resources :players
+  resources :words ,only: [:index, :show]
+  resources :games , only: [:create, :show]
+  resources :players ,only:[:create, :show, :index]
 
   get "/current_player" =>"players#current_player"
+  get "/best_score" =>"players#best_score"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
