5c7253087be5c56bba932374fa8b842d0569e436
Hatice <haticekaraty@gmail.com>
2021-08-27 23:37:51 -0400

    validate username length and format

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ApplicationRecord
     has_many :games
-    validates :name, presence: true , uniqueness: true
+    validates :name, presence: true , uniqueness: true,  length: { in: 3..30 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
