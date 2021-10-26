1d89d0f61e70a75425dbb847e7e744fcb0af3aa6
Hatice <haticekaraty@gmail.com>
2021-10-26 18:05:48 -0400

    deploy at HEROKU

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://naughty-joliot-36b54c.netlify.app'
 
     resource '*',
       headers: :any,
